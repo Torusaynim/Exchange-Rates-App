683d1dd6f8000feea7df7d39b5b68ef79a3b652d
Torusaynim <56824839+Torusaynim@users.noreply.github.com>
2021-11-26 17:57:13 +0300

    Move chart into the exchange page

--- src/exchange.php
+++ src/exchange.php
@@ -4,6 +4,12 @@
   <!-- Meta tags -->
   <title>Bogged Crypto Exchange | Exchange Rates</title>
   <meta charset="utf-8">
+  <style>
+    .chart-container {
+      width: 100%;
+      height: auto;
+    }
+  </style>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -42,9 +48,9 @@
 <main class="flex-shrink-0">
   <div class="container" style="padding-top: 60px">
     <h1 class="mt-5">VIT/RUB - Exchange rates for Vitalium cryptocurrency</h1>
-	<iframe src="chart/linegraph.html" width="100%" height="660px" align="left">
-      Your browser doesn't support iframes!
-    </iframe>
+    <div class="chart-container">
+      <canvas id="mycanvas"></canvas>
+    </div>
 	<p class="lead">
 	  <?php
 	    echo "Trading UI [temporary]";
@@ -60,6 +66,9 @@
   </div>
 </footer>
 
-<script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- javascript -->
+<script type="text/javascript" src="chart/jquery.min.js"></script>
+<script type="text/javascript" src="chart/Chart.min.js"></script>
+<script type="text/javascript" src="chart/linegraph.js"></script>
 </body>
 </html>
