7285242c6a390548832f46606ddf656cdc67256b
Torusaynim <56824839+Torusaynim@users.noreply.github.com>
2021-11-27 02:02:36 +0300

    Chart working properly

--- src/chart/exchangedata.php
+++ src/chart/exchangedata.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 $connection = mysqli_connect("appdb", "user", "password", "appDB") or die(mysqli_error($connection));
 
 //query to get data from the table
-$query = "SELECT price, posted FROM rates";
+$query = "SELECT * FROM (SELECT * FROM rates ORDER BY id DESC LIMIT 10) T ORDER BY id";
 
 //execute query
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
